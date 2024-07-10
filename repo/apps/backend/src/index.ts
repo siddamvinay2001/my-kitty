e4a63c917e8a68784c7d8edb4cdfbb8aa7a9ee1b
siddamvinay2001 <siddamvinay1368@gmail.com>
2024-07-10 15:41:29 +0530

    feat kitty_8: User schema and postgres

--- repo/apps/backend/src/index.ts
+++ repo/apps/backend/src/index.ts
@@ -1,10 +1,19 @@
 import express from 'express';
+import { PrismaClient } from '@prisma/client'
+const prisma = new PrismaClient()
 
 const app = express();
 
-app.get('/', (req, res) => {
-  res.json('Backend');
-});
-app.listen(3003, () => {
-  console.log('Backend is up at 3003');
-});
+async function insertUser(name:string, password: string) {
+  const res = await prisma.user.create({data: {name, password}, select:{id:true,tokens:true}});
+  console.log(res);
+}
+
+insertUser("kitty","WeLoveKitty")
+
+// app.get('/', (req, res) => {
+//   res.json('Backend');
+// });
+// app.listen(3003, () => {
+//   console.log('Backend is up at 3003');
+// });
