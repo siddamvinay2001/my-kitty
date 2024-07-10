58b62c82d509d7849239966d3e35574f92a53653
siddamvinay2001 <siddamvinay1368@gmail.com>
2024-07-10 15:58:28 +0530

    feat kitty_8: Resolved lint check errors

--- repo/apps/backend/src/index.ts
+++ repo/apps/backend/src/index.ts
@@ -1,15 +1,17 @@
-import express from 'express';
-import { PrismaClient } from '@prisma/client'
-const prisma = new PrismaClient()
+// import express from 'express';
+//const app = use express()
+import { PrismaClient } from '@prisma/client';
+const prisma = new PrismaClient();
 
-const app = express();
-
-async function insertUser(name:string, password: string) {
-  const res = await prisma.user.create({data: {name, password}, select:{id:true,tokens:true}});
+async function insertUser(name: string, password: string) {
+  const res = await prisma.user.create({
+    data: { name, password },
+    select: { id: true, tokens: true },
+  });
   console.log(res);
 }
 
-insertUser("kitty","WeLoveKitty")
+insertUser('kitty', 'WeLoveKitty');
 
 // app.get('/', (req, res) => {
 //   res.json('Backend');
