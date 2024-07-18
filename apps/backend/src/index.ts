6a66afa79920cff4317aec2f739accfa22f8a41e
S V S Sudheep Rao <sudheeprao@Ss-MacBook-Air.local>
2024-07-18 22:54:07 +0530

    feat: updated configs

--- apps/backend/src/index.ts
+++ apps/backend/src/index.ts
@@ -10,6 +10,12 @@ const PORT: number | string = process.env.PORT || 3003;
 app.use(bodyParser.json());
 app.use('/api', apiRouter);
 
+app.use((_error: any,_req: any,res: any,_next: any)=>{
+  res.json({
+    msg:"some error"
+  })
+})
+
 app.listen(PORT, () => {
   console.log(`Server is running at ${PORT}`);
 });
